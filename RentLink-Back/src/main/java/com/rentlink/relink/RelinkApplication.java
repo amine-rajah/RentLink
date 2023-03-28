8ead7cd2731354e17172fdf34d52517cc3209488
amine-rajah <mohamedamine.rajah.officiel@gmail.com>
2023-03-28 02:54:33 +0200

    [BACK] setup prject

---
+++ RentLink-Back/src/main/java/com/rentlink/relink/RelinkApplication.java
@@ -0,0 +1,13 @@
+package com.rentlink.relink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelinkApplication.class, args);
+	}
+
+}
---
+++ RentLink-Back/src/test/java/com/rentlink/relink/RelinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentlink.relink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
